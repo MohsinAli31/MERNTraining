cf1315548679b578c27e0b925595767db0b1618b
Mohsin Ali <mohsin.ali@invozone.com>
2023-02-15 10:35:51 +0500

    react form handles

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,7 @@ import Greet from './components/Greet';
 import Welcome from './components/welcome';
 import Message from './components/message';
 import ParentComp from './components/parentComp';
+import FormComp from './components/formComp';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Greet name="Mohsin ALi"><p>this is children</p></Greet>
       <Welcome />
       <Message />
+      <FormComp></FormComp>
     </div>
   );
 }
